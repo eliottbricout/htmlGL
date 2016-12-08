9516fa6f40f119a39697de0abb1232a8912fadcd
eliottbricout <bricouteliott@gmail.com>
2016-12-09 00:08:35 +0100

    je fais des truc

--- javascript/cartable.js
+++ javascript/cartable.js
@@ -52,7 +52,19 @@ function changeclass(){
 
 function changeeleve(){
 	$("#formulairemodifeleve").hide("progress");
-	console.log("eee");
 	$("#formulairemodifeleve").show("progress");
 
+}
+
+function changeemploi(){
+	$("#emploidutemps").hide("progress");
+	$("#emploidutemps").show("progress");
+
+}
+
+function showalert(alert){
+	$("#"+alert).show();
+	setTimeout(function() {
+		$("#"+alert).hide();
+	}, 2000); 
 }
